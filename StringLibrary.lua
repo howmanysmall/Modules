96546394df740db0a2fca45c38525f873c7feaf1
howmanysmall <lhamil29@bruinmail.slcc.edu>
2018-08-29 10:47:59 -0700

    Update StringLibrary.lua

--- StringLibrary.lua
+++ StringLibrary.lua
@@ -1,25 +1,28 @@
 local Resources = require(game:GetService("ReplicatedStorage"):WaitForChild("Resources"))
 local Table = Resources:LoadLibrary("Table")
+local Typer = Resources:LoadLibrary("Typer")
 -- This requires RoStrap. Check it out on GitHub [https://github.com/RoStrap] and Roblox [https://www.roblox.com/library/725884332/RoStrap-Package-Manager].
 -- If you don't want to use RoStrap, just remove the first two lines and replace `return Table.Lock(StringLibrary)` with `return StringLibrary`. But you should use RoStrap.
 
 local StringLibrary = { }
 
+local MATCH = string.match
+local GSUB = string.gsub
+
 -- Credit to Crazyman32 for these next two functions.
-function StringLibrary.GetNumberFromString(String)
+
+StringLibrary.GetNumberFromString = Typer.AssignSignature(Typer.String, function(String)
 	if not String or type(String) ~= "string" then return String and type(String) == "number" and String or 0 end
-	return tonumber(String) or tonumber(String:match("%-?%d+%.?%d*")) or 0
-end
+	return tonumber(String) or tonumber(MATCH(String, "%-?%d+%.?%d*")) or 0
+end)
 
-function StringLibrary.PositiveInteger(String)
-	return String:gsub("%D+", "")
-end
+StringLibrary.PositiveInteger = Typer.AssignSignature(Typer.String, function(String)
+	return GSUB(String, "%D+", "")
+end)
 
-function StringLibrary.ClampNumber(String, Min, Max)
-	if type(Min) == "number" and type(Max) == "number" then
-		local Number = StringLibrary.GetNumberFromString(String)
-		return tostring(math.clamp(Number, Min, Max))
-	end
-end
+StringLibrary.ClampNumber = Typer.AssignSignature(Typer.String, Typer.PositiveInteger, Typer.PositiveInteger, function(String, Min, Max)
+	local Number = StringLibrary.GetNumberFromString(String)
+	return tostring(math.clamp(Number, Min, Max))
+end)
 
 return Table.Lock(StringLibrary)
