a5820a392b61fe900d16070ca1ca582070a7be59
howmanysmall <lhamil29@bruinmail.slcc.edu>
2018-11-20 16:59:25 -0700

    Update assign.lua

--- Otter/assign.lua
+++ Otter/assign.lua
@@ -2,7 +2,7 @@ local function assign(target, ...)
 	for i = 1, select("#", ...) do
 		local source = select(i, ...)
 
-		for key, value in pairs(source) do
+		for key, value in next, source do
 			target[key] = value
 		end
 	end
